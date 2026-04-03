6a9d2bd7635a10c85b39e78be7676c172697cf16
Hfnglos <pohl.julian06@gmail.com>
2026-04-03 20:55:17 +0200

    Corrected Indentation

--- pdf_filewriter/main.py
+++ pdf_filewriter/main.py
@@ -6,40 +6,40 @@ from PySide6.QtWidgets import QTextEdit
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
+        
         self.input = QtWidgets.QLineEdit()
         self.input.setPlaceholderText("random text")
-
+        
         self.button = QtWidgets.QPushButton("Push Button to append text")
         self.text = QtWidgets.QTextEdit()
-
+        
         self.stack = QtWidgets.QStackedLayout()
-
+        
         # Import Page
         importPage = QtWidgets.QWidget()
         importPageLayout = QtWidgets.QVBoxLayout(importPage)
         importPageLayout.addWidget(self.input)
         importPageLayout.addWidget(self.button)
         importPageLayout.addWidget(self.text)
-
+        
         # Config Page
         configPage = QtWidgets.QWidget()
         configPageLayout = QtWidgets.QVBoxLayout(configPage)
         configPageLayout.addWidget(QtWidgets.QLabel("Config Area"))
-
+        
         self.stack.addWidget(importPage)
         self.stack.addWidget(configPage)
-
+        
         self.setLayout(self.stack)
-
+        
         self.button.clicked.connect(lambda: self.stack.setCurrentIndex(1))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-
+    
     widget = MyWidget()
     widget.setWindowTitle("PDF-Filewriter")
     widget.resize(800, 600)
     widget.show()
-
+    
     sys.exit(app.exec())
